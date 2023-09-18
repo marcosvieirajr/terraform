10204bd5710dae7bab31bd1d669b7c336b0ac5d1
Marcos Vieira Jr <marcosvieirajr@gmail.com>
2023-09-18 14:09:48 -0300

    setup aws_vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region     = "us-east-1"
 }
+
+variable vpc_cidr_block {}
+variable env_prefix {}
+
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        Name: "${var.env_prefix}-vpc"
+    }
+}
