5f8679f134438bd45886d22fe04f74a743815e35
Marcos Vieira Jr <marcosvieirajr@gmail.com>
2023-11-26 08:37:15 -0300

    add aws_instance resource

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ output "aws_ami_id" {
     value = data.aws_ami.latest-amazon-linux-image.id
 }
 
-
+variable instance_type {}
 variable public_key_location {}
 
 resource "aws_key_pair" "ssh-key" {
@@ -103,3 +103,15 @@ resource "aws_key_pair" "ssh-key" {
     public_key = file(var.public_key_location)
 }
 
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone = var.avail_zone
+
+    tags = {
+         Name: "${var.env_prefix}-server"
+     }
+}
