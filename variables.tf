ab12b6a42a3463a697975675527f2cbb2e4df4ed
Marcos Vieira Jr <marcosvieirajr@gmail.com>
2024-01-10 08:13:09 -0300

    add variables

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable vpc_cidr_block {}
+variable env_prefix {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable my_ip {}
+variable instance_type {}
+variable public_key_location {}
