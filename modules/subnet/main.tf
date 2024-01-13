c01710198fef5bf64114efac0df099448070301d
Marcos Vieira Jr <marcosvieirajr@gmail.com>
2024-01-13 16:38:46 -0300

    update subnet main file

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.myapp-vpc.id
+    vpc_id = var.vpc_id
     cidr_block = var.subnet_cidr_block
     availability_zone = var.avail_zone
     tags = {
@@ -8,20 +8,20 @@ resource "aws_subnet" "myapp-subnet-1" {
 }
 
 resource "aws_internet_gateway" "myapp-igw" {
-    vpc_id = aws_vpc.myapp-vpc.id
+    vpc_id = var.vpc_id
     tags = {
         Name: "${var.env_prefix}-igw"
     }
 }
 
-resource "aws_route_table" "myapp-route-table" {
-    vpc_id = aws_vpc.myapp-vpc.id
+resource "aws_default_route_table" "main-rtb" {
+    vpc_id = var.default_route_table_id
 
     route {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.myapp-igw.id
     }
     tags = {
-        Name: "${var.env_prefix}-rtb"
+        Name: "${var.env_prefix}-main-rtb"
     }
 }
