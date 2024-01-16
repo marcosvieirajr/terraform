a91109fe154087fdb7b42d2880fe066bca735c1c
Marcos Vieira Jr <marcosvieirajr@gmail.com>
2024-01-16 17:01:09 -0300

    add subnet module to root main file

--- main.tf
+++ main.tf
@@ -9,6 +9,15 @@ resource "aws_vpc" "myapp-vpc" {
     }
 }
 
+module "myapp-subnet" {
+    source = "./modules/subnet"
+    subnet_cidr_block = var.subnet_cidr_block
+    avail_zone = var.avail_zone
+    env_prefix = var.env_prefix
+    vpc_id = aws_vpc.myapp-vpc.id
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+}
+
 resource "aws_route_table_association" "a-rtb-subnet" {
     subnet_id = aws_subnet.myapp-subnet-1.id
     route_table_id = aws_route_table.myapp-route-table.id
