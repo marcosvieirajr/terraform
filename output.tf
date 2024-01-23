2268660e2507deabc268b69f07191e1c19b965e8
Marcos Vieira Jr <marcosvieirajr@gmail.com>
2024-01-23 18:15:34 -0300

    update output webserver module file

--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
-output "aws_ami_id" {
-    value = data.aws_ami.latest-amazon-linux-image.id
-}
-
 output "aws_public_ip" {
-    value = aws_instance.myapp-server.public_ip
+    value = module.myapp-server.instance.public_ip
 }
