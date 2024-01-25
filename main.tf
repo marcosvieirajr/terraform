69b08b8bd21d91e66ed73681aa5938305302bb9b
Marcos Vieira Jr <marcosvieirajr@gmail.com>
2024-01-25 18:20:14 -0300

    add webserver module to main

--- main.tf
+++ main.tf
@@ -17,3 +17,15 @@ module "myapp-subnet" {
     vpc_id = aws_vpc.myapp-vpc.id
     default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
 }
+
+module "myapp-server" {
+    source = "./modules/webserver"
+    vpc_id = aws_vpc.myapp-vpc.id
+    my_ip = var.my_ip
+    env_prefix = var.env_prefix
+    image_name = var.image_name
+    public_key_location = var.public_key_location
+    instance_type = var.instance_type
+    subnet_id = module.myapp-subnet.subnet.id
+    avail_zone = var.avail_zone
+}
