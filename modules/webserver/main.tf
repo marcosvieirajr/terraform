83f22e765c71cf95137d842b3d221123c0f0b848
Marcos Vieira Jr <marcosvieirajr@gmail.com>
2024-02-02 18:32:59 -0300

    add aws_instance

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -44,3 +44,18 @@ resource "aws_key_pair" "ssh-key" {
     key_name = "server-key"
     public_key = file(var.public_key_location)
 }
+
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = var.subnet_id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+
+    tags = {
+         Name: "${var.env_prefix}-server"
+     }
+}
